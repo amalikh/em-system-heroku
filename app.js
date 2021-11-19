8abfeb9478bed506a7ed6c128214cbabcc3b7c49
amalikh <msmartanu@gmail.com>
2021-11-19 15:44:14 +0500

    Configuration to deploy to heroku

--- app.js
+++ app.js
@@ -43,9 +43,9 @@ app.use('/payroll', payrollRoutes);
 const db = require("./models");
 db.sequelize.sync();
 
-// app.get("/", (req, res) => {
-//     res.send("Welcom world...");
-// });
+app.get("/", (req, res) => {
+    res.send("Welcom world...");
+});
 // app.get("/image.png", (req, res) => {
 //     res.sendFile(path.join(__dirname, "./uploads/image.png"));
 //   });
