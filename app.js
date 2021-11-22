04abf551ac7f47246656b98cb4bcb4a2e718d819
amalikh <msmartanu@gmail.com>
2021-11-22 16:28:50 +0500

    message here

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ const leaveRoutes = require('./routes/leave');
 const payrollRoutes = require('./routes/payrollRoutes');
 const roleRoutes = require('./routes/roleRoutes');
 
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-app.use(requireHTTPS);
+// function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
+// app.use(requireHTTPS);
 
 
 const app = express();
